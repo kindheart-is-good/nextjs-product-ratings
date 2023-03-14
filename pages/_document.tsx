42be7cbd9ae6d36304d2eff96cb82ce3bb84d675
kindheart-is-good <oneofmycontacts5@gmail.com>
2023-03-14 21:39:59 +0300

    Add GoogleFonts and CSS global variables

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,9 @@ export default function Document() {
       <Head>
         <title>Top - product ratings</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
