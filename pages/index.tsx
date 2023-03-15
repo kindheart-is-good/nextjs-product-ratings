0aaaa6c9ee5e5c9364ea3f9508359c1f3818e4f4
kindheart-is-good <oneofmycontacts5@gmail.com>
2023-03-15 22:07:12 +0300

    Create Htag component for headers

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
+import { Htag } from '../components';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home(): JSX.Element {
   return (
-    <div>
-      asdasd
-    </div>
+    <>
+      <Htag tag="h1">Test</Htag>
+    </>
   );
 }
