07f7c8144f12b54f74bd999002be705b153d7e3b
kindheart-is-good <oneofmycontacts5@gmail.com>
2023-03-23 22:52:48 +0300

    Create Button component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
-import Head from 'next/head';
-import Image from 'next/image';
 import { Inter } from 'next/font/google';
-import { Htag } from '../components';
+import { Button, Htag } from '../components';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -9,6 +7,8 @@ export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag="h1">Test</Htag>
+      <Button appearance='primary'>Кнопка</Button>
+      <Button appearance='ghost'>Кнопка</Button>
     </>
   );
 }
