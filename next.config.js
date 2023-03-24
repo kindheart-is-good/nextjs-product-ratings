e2946182b7b415c0cb569fdf542685803ee7a238
kindheart-is-good <oneofmycontacts5@gmail.com>
2023-03-24 21:50:58 +0300

    Add ability to work with SVG files.
    Also add SVG icon for Button component.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/, // Если мы видим файл свг
+      use: ["@svgr/webpack"], //то мы с вами используем специальный загрузчик svgr
+    });
+    return config;
+  },
+};
