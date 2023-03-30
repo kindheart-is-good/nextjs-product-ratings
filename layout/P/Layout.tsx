ea583a68378ef58a9b58b6df7be02f03740cd5fb
kindheart-is-good <oneofmycontacts5@gmail.com>
2023-03-30 17:54:45 +0300

    Style: Create adaptive layout using CSS Grid

--- layout/P/Layout.tsx
+++ layout/P/Layout.tsx
@@ -1,5 +1,5 @@
 import { LayoutProps } from './Layout.props';
-import styles from './P.module.css';
+import styles from './Layout.module.css';
 import cn from 'classnames';
 import { Header } from './Header/Header';
 import { Sidebar } from './Sidebar/Sidebar';
@@ -8,16 +8,14 @@ import { FunctionComponent } from 'react';
 
 const Layout = ({children}: LayoutProps): JSX.Element => {
 	return (
-		<>
-			<Header />
-			<div>
-				<Sidebar />
-				<div>
-					{children}
-				</div>
+		<div className={styles.wrapper}>
+			<Header className={styles.header} />
+			<Sidebar className={styles.sidebar} />
+			<div className={styles.body}>
+				{children}
 			</div>
-			<Footer />
-		</>
+			<Footer className={styles.footer} />
+		</div>
 	);
 };
 
